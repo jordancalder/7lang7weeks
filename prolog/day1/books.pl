9f63685f65c6ea1b898aac641203f501a27b66dc
Jordan Calder <jordancalder@gmail.com>
2016-12-18 16:58:49 -0700

    Day 1 prolog

---
+++ prolog/day1/books.pl
@@ -0,0 +1,9 @@
+book(stranger_in_a_strange_land, robert_heinlein).
+book(starship_troopers, robert_heinlein).
+book(the_moon_is_a_harsh_mistress, robert_heinlein).
+book(blood_meridian, cormac_mccarthy).
+book(in_cold_blood, truman_capote).
+book(modern_romance, aziz_ansari).
+book(mans_search_for_meaning, viktor_frankl).
+
+% book(Book, robert_heinlein).
---
+++ prolog/day1/music.pl
@@ -0,0 +1,17 @@
+musician(r_kelly, vocals).
+musician(martin_garrix, synth).
+musician(kygo, synth).
+musician(jimi_hendrix, guitar).
+musician(eric_clapton, guitar).
+musician(jimmy_page, guitar).
+musician(stevie_ray_vauhan, guitar).
+
+genre(edm, martin_garrix).
+genre(house, kygo).
+genre(r_and_b, r_kelly).
+genre(rock, jimi_hendrix).
+genre(rock, eric_clapton).
+genre(blues, stevie_ray_vauhan).
+genre(rock, jimmy_page).
+
+% musician(Musician, guitar).
