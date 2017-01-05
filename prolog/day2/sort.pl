f47099cd8263b4ffa77f6b975ec94c741b20e873
Jordan Calder <jordancalder@gmail.com>
2017-01-05 12:27:07 -0700

    fix sort name

--- prolog/day2/sort.pl
+++ prolog/day2/sort.pl
@@ -1,7 +1,7 @@
 j_sort(List,Sorted) :- compare_sort(List,[],Sorted).
 
 compare_sort([],Empty,Empty).
-compare_sort([Head|Tail],Temp,Sorted) :- list_insert(Head,Temp,Temp2), acc_sort(Tail,Temp2,Sorted).
+compare_sort([Head|Tail],Temp,Sorted) :- list_insert(Head,Temp,Temp2), compare_sort(Tail,Temp2,Sorted).
 
 list_insert(X,[],[X]).
 list_insert(X,[Y|Tail],[Y|NextTail]) :- X >= Y, list_insert(X, Tail, NextTail).
